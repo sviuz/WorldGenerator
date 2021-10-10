148e2579711cddad87dc6d472c9d73a1890a62a7
Danil Kuibida <60036237+sviuz@users.noreply.github.com>
2021-10-10 19:25:28 +0300

    Init

---
+++ Assets/Scripts/Data/MapData.cs
@@ -0,0 +1,14 @@
+namespace Data {
+  public class MapData {
+    public float[,] Data;
+    public float Min { get; set; }
+    public float Max { get; set; }
+
+    public MapData(int width, int height) {
+      Data = new float[width, height];
+      Min = float.MinValue;
+      Max = float.MaxValue;
+    }
+  
+  }
+}
---
+++ Assets/Scripts/Data/Tile.cs
@@ -0,0 +1,10 @@
+﻿namespace Data {
+  public class Tile {
+    public float HeightValue { get; set; }
+    public int X, Y;
+
+    public Tile() {
+      
+    }
+  }
+}
